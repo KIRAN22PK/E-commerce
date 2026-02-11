734a4804a05d2ee812bc32c193153655ede02918
KIRAN22PK <gandhamkiran22pk@gmail.com>
2026-02-11 22:20:20 +0530

    added

--- userback/userback/settings.py
+++ userback/userback/settings.py
@@ -94,7 +94,8 @@ import dj_database_url
 DATABASES = {
     "default": dj_database_url.parse(
         os.environ.get("DATABASE_URL"),
-        conn_max_age=0
+        conn_max_age=0,
+        ssl_require=True
     )
 }
 
