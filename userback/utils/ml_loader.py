f3cd8bd204625db47dd73bd576e35d8d2ad23624
KIRAN22PK <gandhamkiran22pk@gmail.com>
2026-02-11 20:11:44 +0530

    i think last

--- userback/utils/ml_loader.py
+++ userback/utils/ml_loader.py
@@ -1,6 +1,3 @@
-from transformers import T5Tokenizer, T5ForConditionalGeneration
-from sentence_transformers import SentenceTransformer
-
 MODEL_NAME = "google/flan-t5-small"
 EMBEDDING_MODEL_NAME = "all-MiniLM-L6-v2"
 EMBEDDING_DIM = 384
@@ -15,6 +12,8 @@ def get_t5_model():
     global _t5_model, _t5_tokenizer
 
     if _t5_model is None or _t5_tokenizer is None:
+        from transformers import T5Tokenizer, T5ForConditionalGeneration
+        
         _t5_tokenizer = T5Tokenizer.from_pretrained(MODEL_NAME)
         _t5_model = T5ForConditionalGeneration.from_pretrained(MODEL_NAME)
 
@@ -25,6 +24,8 @@ def get_embedding_model():
     global _embedding_model
 
     if _embedding_model is None:
+        from sentence_transformers import SentenceTransformer
+        
         _embedding_model = SentenceTransformer(EMBEDDING_MODEL_NAME)
 
     return _embedding_model
