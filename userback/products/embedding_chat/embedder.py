67bd8433212d2b8848d59a6dda49cb9c5f729d16
KIRAN22PK <gandhamkiran22pk@gmail.com>
2026-02-13 16:45:26 +0530

    solved12

--- userback/products/embedding_chat/embedder.py
+++ userback/products/embedding_chat/embedder.py
@@ -1,8 +1,7 @@
-from utils.ml_loader import get_embedding_model
-
-
+# from utils.ml_loader import get_embedding_model
+from utils.ml_loader import get_embedding
 def embed_texts(texts):
-    embedding_model = get_embedding_model()
+    embedding_model = get_embedding()
     return embedding_model.encode(
         texts,
         convert_to_numpy=True,
