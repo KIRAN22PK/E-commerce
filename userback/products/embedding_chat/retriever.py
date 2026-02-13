6f5ebd779712ffd3a1bbb591c4c65bae93cbae91
KIRAN22PK <gandhamkiran22pk@gmail.com>
2026-02-13 17:50:32 +0530

    added1

--- userback/products/embedding_chat/retriever.py
+++ userback/products/embedding_chat/retriever.py
@@ -1,8 +1,8 @@
 from products.embedding_chat.vector_store import search_vectors
 from .embedder import embed_texts
-from utils.ml_loader import get_embedding
+from utils.ml_loader import get_embedding_model
 
 def retrieve_products(query, top_k=10):
-    model=get_embedding
+    model=get_embedding_model
     query_vector = model.encode(query)
     return search_vectors(query_vector, k=top_k)
